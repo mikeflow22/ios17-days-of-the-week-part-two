58dc89bc7999e07a194182c8a2e9e018ae7024f3
Michael R.F <mrflowers22@uwalumni.com>
2018-01-23 12:30:06 -0700

    added tableview delegate functions

--- i0s days of the week two/DayDetailViewController.swift
+++ i0s days of the week two/DayDetailViewController.swift
@@ -10,17 +10,12 @@ import UIKit
 
 class DayDetailViewController: UIViewController {
 
+    
+    var day = " "
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = day
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
     /*
     // MARK: - Navigation
